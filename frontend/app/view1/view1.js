87a09ef5e563f23b6b76c5b41e603c55d1b896cc
Laith Kelou <laith.kelou@rabobank.nl>
2017-05-19 01:24:27 +0200

    .

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -2,21 +2,37 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
-    $scope.data = {
-        singleSelect: null,
-        multipleSelect: [],
-        option1: 'option-1'
-    };
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+        $scope.test = true;
+        $scope.show = {
+            v1: true,
+            v2: false,
+            v3: false
+        }
+        $scope.keuze = {
+            v1: '',
+            v2: '',
+            v3: ''
+        };
 
-    $scope.forceUnknownOption = function() {
-        $scope.data.singleSelect = 'nonsense';
-    };
-}]);
+        $scope.v1Changed = function (value) {
+            $scope.show.v2 = true;
+            console.log($scope.keuze.v1);
+        }
+
+        $scope.v2Changed = function (value) {
+            $scope.show.v3 = true;
+            console.log($scope.keuze.v2);
+        }
+        $scope.v3Changed = function (value) {
+            $scope.test=false;
+            console.log($scope.keuze.v3);
+        }
+    }]);
